6fd4054d8ff1b579123d77e3b45a50d01d3e4817
wiki_coding <you@example.com>
2017-06-30 17:52:33 +0800

    1

--- 王者荣耀碰碰球/ViewController.m
+++ 王者荣耀碰碰球/ViewController.m
@@ -16,7 +16,7 @@
 @property (nonatomic, strong) UIGravityBehavior *gravity;
 @property (nonatomic, strong) UICollisionBehavior *collision;
 @property (nonatomic, strong) UIDynamicItemBehavior *dynamic;
-@property (nonatomic) CMMotionManager *MotionManager;
+@property (nonatomic) CMMotionManager *motionManager;
 
 @end
 
@@ -66,49 +66,51 @@
     
     self.animator     = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
     
-    //添加重力的动态特性，使其可执行
+    //重力动态特性
     self.gravity = [[UIGravityBehavior alloc]initWithItems:self.balls];
     [self.animator addBehavior:self.gravity];
     
-    //添加碰撞的动态特性，使其可执行
+    //碰撞动态特性
     self.collision = [[UICollisionBehavior alloc]initWithItems:self.balls];
     self.collision.translatesReferenceBoundsIntoBoundary = YES;
     [self.animator addBehavior:self.collision];
     
     //弹性
     self.dynamic = [[UIDynamicItemBehavior alloc] initWithItems:self.balls];
-    self.dynamic.allowsRotation = YES;//允许旋转
+    self.dynamic.allowsRotation = YES;
     self.dynamic.elasticity = 0.6;//弹性
     [self.animator addBehavior:self.dynamic];
 }
 
 - (void)startAnim{
-    //初始化全局管理对象
+  
     
-    self.MotionManager = [[CMMotionManager alloc]init];
-    self.MotionManager.deviceMotionUpdateInterval = 0.01;
+    self.motionManager = [[CMMotionManager alloc]init];
+    self.motionManager.deviceMotionUpdateInterval = 0.01;
     
     
     __weak ViewController *weakSelf = self;
     
-    [self.MotionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMDeviceMotion *_Nullable motion,NSError * _Nullable error) {
+    [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMDeviceMotion *_Nullable motion,NSError * _Nullable error) {
         
         NSString *yaw = [NSString stringWithFormat:@"%f",motion.attitude.yaw];
         NSString *pitch = [NSString stringWithFormat:@"%f",motion.attitude.pitch];
         NSString *roll = [NSString stringWithFormat:@"%f",motion.attitude.roll];
-        
         double rotation = atan2(motion.attitude.pitch, motion.attitude.roll);
         
         //重力角度
         weakSelf.gravity.angle = rotation;
         
-        //        NSLog(@"yaw = %@,pitch = %@, roll = %@,rotation = %fd",yaw,pitch,roll,rotation);
-        
     }];
     
 }
 
-
+- (void)dealloc{
+    
+    [self.motionManager stopDeviceMotionUpdates];
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
